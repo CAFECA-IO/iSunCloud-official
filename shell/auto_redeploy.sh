7a250a35bb288ab051ff7e1ccf69ebb9b5c24245
Luphia Chang <luphia@mermer.cc>
2025-12-23 11:33:33 +0800

    Use npm run swarm to replace npm run production

--- shell/auto_redeploy.sh
+++ shell/auto_redeploy.sh
@@ -26,7 +26,7 @@ if [ "$LOCAL" != "$REMOTE" ]; then
   
   echo "Restarting application..."
   # 使用 restart 比 delete + start 更平滑，若不存在則會報錯但不影響流程
-  pm2 restart iSunCloud || pm2 start npm --name iSunCloud -- run production
+  pm2 restart iSunCloud || pm2 start npm --name iSunCloud -- run swarm
 else
   echo "[$(date)] No new commits."
 fi
@@ -34,5 +34,5 @@ fi
 # 使用 pm2 jlist 輸出 JSON 並用 grep 檢查是否已啟動
 if ! pm2 list | grep -q "iSunCloud" || ! pm2 list | grep "iSunCloud" | grep -q "online"; then
   echo "Application is not running. Starting now..."
-  pm2 start npm --name iSunCloud -- run production
+  pm2 start npm --name iSunCloud -- run swarm
 fi
